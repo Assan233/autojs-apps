20f570c39a729d75c35715ae2a5fa3286759401b
Assan <719685838a@gmail.com>
2024-02-27 23:30:10 +0800

    feat: 打开地铁码

--- work/subway-card/subwayCard.js
+++ work/subway-card/subwayCard.js
@@ -1,2 +1,15 @@
-function main() {}
+/**
+ * 调用地铁二维码 脚本
+ */
+function main() {
+  // 返回桌面
+  home();
+  sleep(1000);
+
+  // 长按 厦门地铁软件
+  longClick(635, 2205);
+
+  // 点击交通码
+  text("厦门交通码").findOne(2000).parent().click();
+}
 main();
